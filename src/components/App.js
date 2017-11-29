0298851157b72ee43d18e1d474dbaf6fc539fc65
jayherring <j.herring.8989@gmail.com>
2017-11-28 22:00:11 -0600

    working version

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './header';
 import Footer from './footer';
 import Home from './home';
+import Teams from './teams'
+import Team from './team'
 
 
 
@@ -18,6 +20,8 @@ class App extends Component {
 			<div>
 				<Header/>
 				<Route exact path="/" component={Home}/>
+				<Route exact path="/teams" component={Teams}/>
+				<Route exact path="/team/:id" component={Team}/>
 				<Footer/>
 
 			</div>
--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 //comps
 import Featured from './featured';
 import Subscriptions from './subscriptions'
+import Blocks from './blocks'
+import Poll from './poll'
+
 
 const URL_HOME = `http://localhost:3004/home`
 
@@ -34,6 +37,8 @@ class Home extends Component {
 				
 				<Featured slides={this.state.home.slider}/>
 				<Subscriptions/>
+				<Blocks blocks={this.state.home.blocks}/>
+				<Poll/>
 			</div>
 		)
 	}
